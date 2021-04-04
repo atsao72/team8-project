086927700760a419692c722a886f191a32f9f23f
Heysil Baez <hb365@cornell.edu>
2021-04-03 23:14:13 -0400

    Creates dummy file to display

---
+++ src/main/java/com/google/sps/servlets/DummyFileCreator.java
@@ -0,0 +1,37 @@
+package com.google.sps.servlets;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+/** Servlet responsible for creating new tasks. */
+@WebServlet("/new-dummy-file")
+public class NewDummyFileServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Sanitize user input to remove HTML tags and JavaScript.
+    String fileName = Jsoup.clean(request.getParameter("fileName"), Whitelist.none());
+    long timestamp = System.currentTimeMillis();
+
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("File");
+    FullEntity fileEntity =
+        Entity.newBuilder(keyFactory.newKey())
+            .set("fileName", fileName)
+            .set("timestamp", timestamp)
+            .build();
+    datastore.put(fileEntity);
+
+    response.sendRedirect("/index.html");
+  }
+}
